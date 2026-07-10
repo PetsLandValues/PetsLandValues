11c34ec2f6fb933b8fe0454bb8bb127102bb0497
PetsLandValues <nervees12@gmail.com>
2026-07-11 00:57:22 +0500

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,81 @@
+const siteInfo = {
+
+    name:"Pet Values",
+
+    version:"1.0.0",
+
+    lastUpdate:"2026-07-11"
+
+};
+
+
+
+const pets = [
+
+
+{
+
+    id:"mint-chip-lemur",
+
+    name:"Mint Chip Lemur",
+
+    image:"images/pets/mint-chip-lemur.png",
+
+    rarity:"Legendary",
+
+
+    values:{
+
+        normal:400,
+
+        vintage:1600,
+
+        neon:4000
+
+    },
+
+
+    demand:4,
+
+
+    released:"2026-07-08",
+
+    updated:"2026-07-11"
+
+},
+
+
+
+{
+
+    id:"chocobara",
+
+    name:"Chocobara",
+
+    image:"images/pets/chocobara.png",
+
+    rarity:"Rare",
+
+
+    values:{
+
+        normal:650,
+
+        vintage:2600,
+
+        neon:6500
+
+    },
+
+
+    demand:4,
+
+
+    released:"2026-07-08",
+
+    updated:"2026-07-11"
+
+}
+
+
+];
