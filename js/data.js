f1d26de145a77ed2276e04e165a982c3015592d2
PetsLandValues <nervees12@gmail.com>
2026-07-11 00:24:05 +0500

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,33 @@
+const pets = [
+{
+    id: 1,
+
+    name: "Mint Chip Lemur",
+
+    image: "images/pets/mint-chip-lemur.png",
+
+    rarity: "Legendary",
+
+    value: 400,
+    vintage: 1600,
+    neon: 4000,
+
+    demand: 4
+},
+
+{
+    id: 2,
+
+    name: "Chocobara",
+
+    image: "images/pets/Chocobara.png",
+
+    rarity: "Rare",
+
+    value: 15,
+    vintage: 68,
+    neon: 148,
+
+    demand: 2
+}
+];
